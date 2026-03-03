c2c2aa46734d9ec37feb93191820b6532c76226d
oluwasegun-ayo <adebayooluwasegun429@gmail.com>
2026-03-03 13:31:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
-const express = require("express");
 const { Pool } = require("pg");
 
-const app = express();
+let pool;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
-
-app.use(express.static("public"));
-
-app.get("/", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.sendFile(__dirname + "/public/index.html");
-  } catch (error) {
-    console.error(error);
-    res.send("Database connection failed ❌");
-  }
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+if (process.env.DATABASE_URL) {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  });
+} else {
+  console.log("DATABASE_URL not found");
+}
