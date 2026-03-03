56face16b3bee14d22d8a20cea119967a06232b1
oluwasegun-ayo <adebayooluwasegun429@gmail.com>
2026-03-03 22:19:59 +0100

    Remove PostgreSQL connection setup from server.js
    Removed PostgreSQL Pool configuration and related code.

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 const express = require("express");
 const path = require("path");
-const { Pool } = require("pg");
 
 const app = express();
 
-// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// Root route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-
 const PORT = process.env.PORT || 10000;
+
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
