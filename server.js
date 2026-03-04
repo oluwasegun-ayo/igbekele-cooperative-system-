1756a8190a64bd5770d704206255c57965f53ffa
oluwasegun-ayo <adebayooluwasegun429@gmail.com>
2026-03-04 11:04:55 +0100

    Fix app.listen formatting and remove redundant line

--- server.js
+++ server.js
@@ -10,7 +10,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 10000;
-
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+app.listen(PORT, () => console.log("Server running on port " + PORT));
