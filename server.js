0f5cefd7128af1d0a2684d92a7e627532863a503
oluwasegun-ayo <adebayooluwasegun429@gmail.com>
2026-03-04 14:17:02 +0100

    Add route to serve dashboard.html

--- server.js
+++ server.js
@@ -11,3 +11,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log("Server running on port " + PORT));
+app.get("/dashboard", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "dashboard.html"));
+});
