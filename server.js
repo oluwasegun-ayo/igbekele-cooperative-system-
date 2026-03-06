6e9148faa0bf240f6645d918ed6816e7f672e58f
oluwasegun-ayo <adebayooluwasegun429@gmail.com>
2026-03-06 11:46:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+console.log("Server running on port " + PORT);
 });
